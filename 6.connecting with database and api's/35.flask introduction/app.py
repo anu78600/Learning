18287d07779df5206057054b1ff28895848a9c13
anu78600 <anupam78600@gmail.com>
2023-12-11 18:38:41 +0530

    add initials files

---
+++ 6.connecting with database and api's/35.flask introduction/app.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template,request,jsonify
+
+app = Flask(__name__)
+
+''' yek form hoga jha pr data ko input krenge , 
+ar vo data ko form ke through pass karna chahiye python code ke andr'''
+
+@app.route('/',methods=['GET','POST'])
+def home_page():
+    return render_template('index.html')
+
+
+@app.route("/")
+def hello_world():
+    return "<h1>Hello World</h1"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
