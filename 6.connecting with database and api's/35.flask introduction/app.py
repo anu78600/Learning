1d456df350762a2772c2aeaff54c83b000729460
anu78600 <anupam78600@gmail.com>
2023-12-12 20:12:44 +0530

    intialize code

--- 6.connecting with database and api's/35.flask introduction/app.py
+++ 6.connecting with database and api's/35.flask introduction/app.py
@@ -9,10 +9,32 @@ ar vo data ko form ke through pass karna chahiye python code ke andr'''
 def home_page():
     return render_template("index.html")
 
+@app.route('/math',methods=['POST'])
+def math_operator():   # client side se jo v input aayega vo yha capture hoga
+    if(request.method == 'POST'):
+        ops = request.form['operation']
+        num1 = int(request.form['num1'])
+        num2 = int(request.form['num2'])
+        if(ops == 'add'):
+            r = num1 + num2
+            result = 'the sum of ' + str(num1) + ' and ' + str(num2) + " is " + str(r)
+         
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello World</h1"
+        
+        if(ops == 'subtract'):
+            r = num1 - num2
+            result = 'the difference of '+ str(num1) +' and '+ str(num2) + " is " + str(r)
+
+        if(ops == 'divide'):
+            r = num1 / num2
+            result = 'the division of '+ str(num1) +' and '+ str(num2) + " is " + str(r)
+
+        if(ops == 'multiply'): 
+            r = num1 * num2
+            result = 'the multiplication of '+ str(num1) +' and '+ str(num2) + " is " + str(r)
+
+          
+        return render_template('results.html', result = result)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
