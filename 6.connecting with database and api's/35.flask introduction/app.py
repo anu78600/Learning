41edc8fa6a54209082838430d5047f8cf007165f
anu78600 <anupam78600@gmail.com>
2023-12-12 20:55:04 +0530

    learning postman tool

--- 6.connecting with database and api's/35.flask introduction/app.py
+++ 6.connecting with database and api's/35.flask introduction/app.py
@@ -36,5 +36,38 @@ def math_operator():   # client side se jo v input aayega vo yha capture hoga
           
         return render_template('results.html', result = result)
 
+@app.route('/postman_data',methods=['POST'])
+def math_operato1():   # upr vale code ko ham dusre tool se bhi operate karne ko kosis krenge , postman
+    if(request.method == 'POST'):
+        ops = request.json['operation']
+        num1 = int(request.json['num1'])
+        num2 = int(request.json['num2'])
+        if(ops == 'add'):
+            r = num1 + num2
+            result = 'the sum of ' + str(num1) + ' and ' + str(num2) + " is " + str(r)
+         
+
+        
+        if(ops == 'subtract'):
+            r = num1 - num2
+            result = 'the difference of '+ str(num1) +' and '+ str(num2) + " is " + str(r)
+
+        if(ops == 'divide'):
+            r = num1 / num2
+            result = 'the division of '+ str(num1) +' and '+ str(num2) + " is " + str(r)
+
+        if(ops == 'multiply'): 
+            r = num1 * num2
+            result = 'the multiplication of '+ str(num1) +' and '+ str(num2) + " is " + str(r)
+
+          
+        return jsonify(result)
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
+
+# GET means sending data through url
+# POST means sending data through body 
+# # post is more secure than get 
+# post me koi data dekh nhi skta h, get me data ko dekh skte h   
